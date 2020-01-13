55747dc622ee3895fd00deb1d6475128b4e7c6a0
Ilya Pavlov <ilya.pavlov@list.ru>
2020-01-13 22:48:01 +0300

    4.5. Отладка программы в IDEA[#179225]

---
+++ chapter_001/src/test/java/ru/job4j/condition/SqMaxTest.java
@@ -0,0 +1,84 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SqMaxTest {
+
+    @Test
+    public void whenFirstMax() {
+
+        int first = 4;
+        int second = 1;
+        int third = 1;
+        int forth = 1;
+
+        int expected = 4;
+
+        int result = SqMax.max(first, second, third, forth);
+        assertThat(result, is(expected));
+    }
+
+
+    @Test
+    public void whenSecondMax() {
+
+        int first = 1;
+        int second = 4;
+        int third = 1;
+        int forth = 1;
+
+        int expected = 4;
+
+        int result = SqMax.max(first, second, third, forth);
+        assertThat(result, is(expected));
+    }
+
+
+    @Test
+    public void whenThirdMax() {
+
+        int first = 1;
+        int second = 1;
+        int third = 4;
+        int forth = 1;
+
+        int expected = 4;
+
+        int result = SqMax.max(first, second, third, forth);
+        assertThat(result, is(expected));
+    }
+
+
+    @Test
+    public void whenForthMax() {
+
+        int first = 1;
+        int second = 1;
+        int third = 1;
+        int forth = 4;
+
+        int expected = 4;
+
+        int result = SqMax.max(first, second, third, forth);
+        assertThat(result, is(expected));
+    }
+
+
+    @Test
+    public void whenAllTheSame() {
+
+        int first = 4;
+        int second = 4;
+        int third = 4;
+        int forth = 4;
+
+        int expected = 4;
+
+        int result = SqMax.max(first, second, third, forth);
+        assertThat(result, is(expected));
+    }
+
+
+}
