983c6330f9d8627bd53657a752a2467ff5bdf6e4
Ilya Pavlov <ilya.pavlov@list.ru>
2020-02-10 16:59:01 +0300

    2.2. Рефакторинг - Площадь треугольника.[#179354]

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -1,7 +1,77 @@
 package ru.job4j.condition;
 
 public class Triangle {
-    public static boolean exist(double ab, double ac, double bc) {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    /**
+     * Метод вычисления периметра по длинам сторон.
+     *
+     * Формула.
+     *
+     * (a + b + c) / 2
+     *
+     * @param a расстояние между точками a b
+     * @param b расстояние между точками a c
+     * @param c расстояние между точками b c
+     * @return Периметр.
+     */
+    public double period(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+
+    /**
+     * Метод должен вычислить площадь треугольника.
+     *
+     * Формула.
+     *
+     * √ p *(p - ab) * (p - ac) * (p - bc)
+     *
+     * где √ - корень квадратный, для извлечения корня использовать метод Math.sqrt().
+     *
+     * @return Вернуть площадь, если треугольник существует или -1.
+     */
+    public double area() {
+        double rsl = -1;
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
+        double p = period(a, b, c);
+        if (this.exist(a, b, c)) {
+            rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        return rsl;
+    }
+
+    /**
+     * Метод проверяет можно ли построить треугольник с такими длинами сторон.
+     *
+     * Подумайте какое надо написать условие, чтобы определить можно ли построить треугольник.
+     *
+     * @param ab Длина от точки a b.
+     * @param ac Длина от точки a c.
+     * @param bc Длина от точки b c.
+     * @return
+     */
+    private boolean exist(double ab, double ac, double bc) {
         return ((ab + ac) > bc) && ((ac + bc) > ab) && ((ab + bc) > ac);
     }
+
+    public static void main(String[] args) {
+        Point a = new Point(-1, -3);
+        Point b = new Point(3, 4);
+        Point c = new Point(5, -5);
+        Triangle t = new Triangle(a, b, c);
+
+        double area = t.area();
+        System.out.println(area);
+    }
+
 }
