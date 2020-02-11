6aab9802ae82c5a0d2c755d6a6babf3738e23ff3
Ilya Pavlov <ilya.pavlov@list.ru>
2020-02-11 23:31:31 +0300

    3. Массивы и модели.[#179377]

---
+++ chapter_001/src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,40 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book book0 = new Book("Zolotoi Telenok", 500);
+        Book book1 = new Book("Clean code", 1000);
+        Book book2 = new Book("Transerfing realnosti", 600);
+        Book book3 = new Book("Opit duraka", 400);
+
+        Book[] books = new Book[4];
+
+        books[0] = book0;
+        books[1] = book1;
+        books[2] = book2;
+        books[3] = book3;
+
+        for (int index = 0; index < books.length; index++) {
+            Book bk = books[index];
+            System.out.println(bk.getName() + " - " + bk.getPages());
+        }
+
+        System.out.println("Exchange books[0] and books[0].");
+        Book tmp = books[0];
+        books[0] = books[2];
+        books[2] = tmp;
+
+        for (int index = 0; index < books.length; index++) {
+            Book bk = books[index];
+            System.out.println(bk.getName() + " - " + bk.getPages());
+        }
+
+        System.out.println("Shown only Clean code books");
+        for (int index = 0; index < books.length; index++) {
+            Book bk = books[index];
+            if (bk.getName() == "Clean code") {
+                System.out.println(bk.getName() + " - " + bk.getPages());
+            }
+        }
+    }
+}
