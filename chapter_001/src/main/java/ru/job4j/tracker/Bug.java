90405fd71b19f7b67a374f4b90985cbc4bcda656
Ilya Pavlov <ilya.pavlov@list.ru>
2020-02-12 11:52:25 +0300

    3. Удаление моделей из массива.[#179379]

--- chapter_001/src/main/java/ru/job4j/tracker/Bug.java
+++ chapter_001/src/main/java/ru/job4j/tracker/Bug.java
@@ -2,6 +2,7 @@ package ru.job4j.tracker;
 
 public class Bug extends Item {
     public Bug(String name) {
-        super(name);
+        super(); // конструктор наследник должен вызывать любой из конструкторов родителя
+        System.out.println("load Bug");
     }
 }
--- chapter_001/src/main/java/ru/job4j/tracker/Item.java
+++ chapter_001/src/main/java/ru/job4j/tracker/Item.java
@@ -2,9 +2,16 @@ package ru.job4j.tracker;
 
 public class Item {
 
-    public Item(String name) {
+    public Item() {
+        System.out.println("load Item");
     }
+
+    public Item(String name) {
+        System.out.println("load Item with String");
+    }
+
     public static void main(String[] args) {
         Item item = new Item("");
+        Bug bug = new Bug("bug");
     }
 }
