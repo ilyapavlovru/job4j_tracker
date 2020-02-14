bf09b86f98f2c80b1c732230968edf9365b593e2
Ilya Pavlov <ilya.pavlov@list.ru>
2020-02-14 15:29:48 +0300

    3. Интерфейс Input [#181092]

---
+++ chapter_002/src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,23 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+
+    private double volume;
+    private double passengers;
+
+    @Override
+    public void drive() {
+        volume-=10;
+    }
+
+    @Override
+    public void passengers(int numPassengers) {
+        passengers += numPassengers;
+    }
+
+    @Override
+    public double refuel(double fuelQty) {
+        volume += fuelQty;
+        return fuelQty * 10;
+    }
+}
