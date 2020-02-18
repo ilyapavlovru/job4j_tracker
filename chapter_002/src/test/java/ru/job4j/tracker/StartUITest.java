a0ed2f96db10594fa720721c2695f898e8637e8c
Ilya Pavlov <ilya.pavlov@list.ru>
2020-02-18 15:07:20 +0300

    9. Написать тесты на StartUI.[#239907]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -7,6 +7,16 @@ import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.*;
 
 public class StartUITest {
+    @Test
+    public void whenExit() {
+        StubInput input = new StubInput(
+                new String[] {"0"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] { action });
+        assertThat(action.isCall(), is(true));
+    }
+
 //    @Test
 //    public void whenAddItem() {
 //        String[] answers = {"Fix PC"};
