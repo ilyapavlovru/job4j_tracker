0a309e32d7fafd41a6ee64766b0e2e522a85ec19
Ilya Pavlov <ilya.pavlov@list.ru>
2020-03-11 17:10:52 +0300

    Подготовка к экзамену по ООП: добавлены еще примеры в пакет interview

--- chapter_002/src/main/java/ru/job4j/interview/constructor/PetCatJavastudy/TestInitOrder.java
+++ chapter_002/src/main/java/ru/job4j/interview/constructor/PetCatJavastudy/TestInitOrder.java
@@ -3,6 +3,7 @@ package ru.job4j.interview.constructor.PetCatJavastudy;
 public class TestInitOrder {
 
     public static void main(String[] args) {
+        System.out.print("pre ");
         Cat cat = new Cat("Rizhick");
     }
 }
