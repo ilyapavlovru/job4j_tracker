eb3d191d988530fcef64f09039d6b690e8164693
Ilya Pavlov <ilya.pavlov@list.ru>
2020-05-07 10:10:16 +0300

    Отсортировать департаменты[#239852]. Исправил сортировку по убыванию - сокращено количество кода

--- chapter_003/src/main/java/ru/job4j/collection/DepDescComp.java
+++ chapter_003/src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,23 +5,7 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        String firstElInO1;
-        if(o1.contains("/")) {
-            firstElInO1 = o1.substring(0, o1.indexOf('/'));
-        } else {
-            firstElInO1 = o1;
-        }
-        String firstElInO2;
-        if(o2.contains("/")) {
-            firstElInO2 = o2.substring(0, o2.indexOf('/'));
-        } else {
-            firstElInO2 = o2;
-        }
-
-        if ((!firstElInO1.equals(firstElInO2)) || (!o1.contains("/") && !o2.contains("/"))) {
-            return o2.compareTo(o1);
-        } else {
-            return o1.compareTo(o2);
-        }
+        int tmp = o2.split("/")[0].compareTo(o1.split("/")[0]);
+        return tmp == 0 ? o1.compareTo(o2) : tmp;
     }
 }
