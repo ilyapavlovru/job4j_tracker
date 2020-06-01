eb92fcc2f3b55ca6d3c6bcfcbb4c592fc7dfefb3
Ilya Pavlov <ilya.pavlov@list.ru>
2020-06-01 18:21:37 +0300

    6. Тестовое задание из модуля коллекции Lite переделать на Stream API. [#239719]

---
+++ chapter_004/src/main/java/ru/job4j/stream/bank/BankService.java
@@ -0,0 +1,75 @@
+package ru.job4j.stream.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<Account>());
+    }
+
+    public void addAccount(String passport, Account account) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            List<Account> accounts = users.get(user);
+            if (!accounts.contains(account)) {
+                accounts.add(account);
+            }
+        }
+    }
+
+    public User findByPassport(String passport) {
+        return users.keySet().stream()
+                .filter(user -> user.getPassport().equals(passport))
+                .findAny()
+                .orElse(null);
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        Account rst = null;
+        User user = findByPassport(passport);
+        if (user != null) {
+            List<Account> accounts = users.get(user);
+            rst = accounts.stream()
+                    .filter(account -> account.getRequisite().equals(requisite))
+                    .findAny()
+                    .orElse(null);
+        }
+        return rst;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        boolean rsl = false;
+
+        Account srcAccount = findByRequisite(srcPassport, srcRequisite);
+        Account destAccount = findByRequisite(destPassport, destRequisite);
+
+        if (srcAccount == null || destAccount == null) {
+            return false;
+        }
+
+        double srcNewBalance = srcAccount.getBalance() - amount;
+        if (srcNewBalance >= 0) {
+            srcAccount.setBalance(srcNewBalance);
+            destAccount.setBalance(destAccount.getBalance() + amount);
+            rsl = true;
+        }
+
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        List<Account> accounts = new ArrayList<>();
+        String requisite = "3fdsbb9";
+        accounts.add(new Account("3fdsbb9", 140));
+        int index = accounts.indexOf(new Account(requisite, -1));
+        Account find = accounts.get(index);
+        System.out.println(find.getRequisite() + " -> " + find.getBalance());
+    }
+}
