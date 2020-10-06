707f0377ff79790e8a02af2ef9bc79097d9f26ae
Ilya Pavlov <ilya.pavlov@list.ru>
2020-10-06 11:17:23 +0300

    1.1. Состояние нити. [#345901]. Add println for better understanding

--- chapter_007/src/main/java/ru/job4j/concurrent/ThreadState.java
+++ chapter_007/src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -2,31 +2,33 @@ package ru.job4j.concurrent;
 
 public class ThreadState {
     public static void main(String[] args) {
+        System.out.println("Main thread started...");
         Thread first = new Thread(
                 () -> {
-                    System.out.println("Creating first thread");
-                    System.out.println(Thread.currentThread().getName());
+                    System.out.println("Creating first thread...name: " + Thread.currentThread().getName());
                 }
                 );
-        System.out.println(first.getState());
+        System.out.println("state of first: " + first.getState());
         first.start();
 
         Thread second = new Thread(
                 () -> {
-                    System.out.println("Creating second thread");
-                    System.out.println(Thread.currentThread().getName());
+                    System.out.println("Creating second thread...name: " + Thread.currentThread().getName());
                 }
         );
-        System.out.println(second.getState());
+        System.out.println("state of second: " + second.getState());
         second.start();
 
-
-
-        while ((first.getState() != Thread.State.TERMINATED) && (second.getState() != Thread.State.TERMINATED)) {
-            System.out.println(first.getState());
-            System.out.println(second.getState());
+        while ((first.getState() != Thread.State.TERMINATED) &&
+                (second.getState() != Thread.State.TERMINATED)) {
+            System.out.println("in while:");
+            System.out.println("state of first: " + first.getState());
+            System.out.println("state of second: " + second.getState());
         }
-        System.out.println(first.getState());
-        System.out.println(second.getState());
+
+        System.out.println("after while:");
+        System.out.println("state of first: " + first.getState());
+        System.out.println("state of second: " + second.getState());
+        System.out.println("Main thread finished...");
     }
 }
