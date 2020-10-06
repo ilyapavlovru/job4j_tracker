86950733549c27f4d4780c3697d6ed086a802f67
Ilya Pavlov <ilya.pavlov@list.ru>
2020-10-06 10:57:48 +0300

    1.1. Состояние нити. [#345901]

---
+++ chapter_007/src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,32 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> {
+                    System.out.println("Creating first thread");
+                    System.out.println(Thread.currentThread().getName());
+                }
+                );
+        System.out.println(first.getState());
+        first.start();
+
+        Thread second = new Thread(
+                () -> {
+                    System.out.println("Creating second thread");
+                    System.out.println(Thread.currentThread().getName());
+                }
+        );
+        System.out.println(second.getState());
+        second.start();
+
+
+
+        while ((first.getState() != Thread.State.TERMINATED) && (second.getState() != Thread.State.TERMINATED)) {
+            System.out.println(first.getState());
+            System.out.println(second.getState());
+        }
+        System.out.println(first.getState());
+        System.out.println(second.getState());
+    }
+}
