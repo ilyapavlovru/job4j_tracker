be3e4a87de00b422aee696727a7db71046eaa7c5
Ilya Pavlov <ilya.pavlov@list.ru>
2020-10-26 10:56:02 +0300

    0. Объект монитор, критическая секция [#345918]

---
+++ chapter_007/src/main/java/ru/job4j/synchronize/Buffer.java
@@ -0,0 +1,19 @@
+package ru.job4j.synchronize;
+
+public class Buffer {
+    private StringBuilder buffer = new StringBuilder();
+
+    public void add(int value) {
+        synchronized(this) {
+            System.out.println(value);
+            buffer.append(value);
+        }
+    }
+
+    @Override
+    public String toString() {
+        synchronized(this) {
+            return buffer.toString();
+        }
+    }
+}
