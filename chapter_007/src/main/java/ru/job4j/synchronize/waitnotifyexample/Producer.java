8b5b068e77884691e715168211979e269aa78b7b
Ilya Pavlov <ilya.pavlov@list.ru>
2020-11-01 16:48:03 +0300

    Методы wait и notify. Источник: https://metanit.com/java/tutorial/8.5.php

---
+++ chapter_007/src/main/java/ru/job4j/synchronize/waitnotifyexample/Producer.java
@@ -0,0 +1,17 @@
+package ru.job4j.synchronize.waitnotifyexample;
+
+// Класс Производитель
+public class Producer implements Runnable {
+    Store store;
+
+    public Producer(Store store) {
+        this.store = store;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i < 6; i++) {
+            store.put();
+        }
+    }
+}
---
+++ chapter_007/src/main/java/ru/job4j/synchronize/waitnotifyexample/Store.java
@@ -0,0 +1,32 @@
+package ru.job4j.synchronize.waitnotifyexample;
+
+// класс Магазин, хранящий произведенные товары
+public class Store {
+    private int product = 0;
+
+    public synchronized void get() {
+        while (product < 1) {
+            try {
+                wait();
+            } catch (InterruptedException e){
+            }
+        }
+        product--;
+        System.out.println("Покупатель купил 1 товар");
+        System.out.println("Продуктов на складе: " + product);
+        notify();
+    }
+
+    public synchronized void put() {
+        while (product >= 3) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+            }
+        }
+        product++;
+        System.out.println("Производитель добавил 1 товар");
+        System.out.println("Товаров на складе: " + product);
+        notify();
+    }
+}
