1cb4c70fa09d58edb3f1bd49af2e84c560d50b7d
Ilya Pavlov <ilya.pavlov@list.ru>
2020-11-04 19:44:45 +0300

    2. Обеспечить остановку потребителя.[66825#345895]

---
+++ chapter_007/src/main/java/ru/job4j/synchronize/waitnotifyqueue2/ParallelSearch.java
@@ -0,0 +1,35 @@
+package ru.job4j.synchronize.waitnotifyqueue2;
+
+public class ParallelSearch {
+    public static void main(String[] args) {
+
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>();
+
+        // потребитель пытается получтить элементы пока значение флага общего ресурса равно true
+        final Thread consumer = new Thread(
+                () -> {
+                    while (queue.isFlag()) {
+                        System.out.println(Thread.currentThread().getName() + ": Consumer пытается извлечь элемент..");
+                        queue.poll();
+                    }
+                }
+        );
+        consumer.start();
+
+        // производитель 3 раза добавляет число в очередь
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        queue.offer(index);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    queue.off();
+                }
+
+        ).start();
+    }
+}
