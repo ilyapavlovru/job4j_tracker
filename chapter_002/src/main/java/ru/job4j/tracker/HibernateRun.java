68c1656119363fd8414a948e972bee74bb0cfd56
Ilya Pavlov <ilya.pavlov@list.ru>
2021-05-07 16:52:32 +0300

    1. HbmTracker.[287705#345815]

--- chapter_002/src/main/java/ru/job4j/tracker/HibernateRun.java
+++ chapter_002/src/main/java/ru/job4j/tracker/HibernateRun.java
@@ -1,10 +1,10 @@
 package ru.job4j.tracker;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import ru.job4j.tracker.model.Item;
+import ru.job4j.tracker.store.HbmTracker;
+import ru.job4j.tracker.store.Store;
 
 import java.sql.Timestamp;
 import java.util.List;
@@ -14,17 +14,27 @@ public class HibernateRun {
         final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
                 .configure().build();
         try {
-            SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-            Item item = create(new Item("Learn Hibernate 2", "Description for Learn Hibernate 2",
-                    new Timestamp(1459510232000L)), sf);
+            Store store = new HbmTracker();
+
+            Item item = new Item("Learn Hibernate 3", "Description for Learn Hibernate 3",
+                    new Timestamp(1459510232000L));
+            Item savedItem = store.add(item);
+            System.out.println(savedItem);
+
+            item.setName("Learn Hibernate 4.");
+            store.replace(savedItem.getId(), item);
             System.out.println(item);
-            item.setName("Learn Hibernate 5.");
-            update(item, sf);
-            System.out.println(item);
-            Item rsl = findById(item.getId(), sf);
+
+            Item rsl = store.findById(item.getId());
             System.out.println(rsl);
-            delete(rsl.getId(), sf);
-            List<Item> list = findAll(sf);
+            store.delete(rsl.getId());
+
+            List<Item> items = store.findByName("Learn Hibernate 4.");
+            for (Item it : items) {
+                System.out.println(it);
+            }
+
+            List<Item> list = store.findAll();
             for (Item it : list) {
                 System.out.println(it);
             }
@@ -33,50 +43,5 @@ public class HibernateRun {
         } finally {
             StandardServiceRegistryBuilder.destroy(registry);
         }
-    }
-
-    public static Item create(Item item, SessionFactory sf) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.save(item);
-        session.getTransaction().commit();
-        session.close();
-        return item;
-    }
-
-    public static void update(Item item, SessionFactory sf) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.update(item);
-        session.getTransaction().commit();
-        session.close();
-    }
-
-    public static void delete(Integer id, SessionFactory sf) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Item item = new Item(null);
-        item.setId(id);
-        session.delete(item);
-        session.getTransaction().commit();
-        session.close();
-    }
-
-    public static List<Item> findAll(SessionFactory sf) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List result = session.createQuery("from ru.job4j.tracker.Item").list();
-        session.getTransaction().commit();
-        session.close();
-        return result;
-    }
-
-    public static Item findById(Integer id, SessionFactory sf) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Item result = session.get(Item.class, id);
-        session.getTransaction().commit();
-        session.close();
-        return result;
     }
 }
---
+++ chapter_002/src/main/java/ru/job4j/tracker/store/HbmTracker.java
@@ -0,0 +1,87 @@
+package ru.job4j.tracker.store;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import ru.job4j.tracker.model.Item;
+
+import javax.persistence.Query;
+import java.util.List;
+
+public class HbmTracker implements Store, AutoCloseable {
+    private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure().build();
+    private final SessionFactory sf = new MetadataSources(registry)
+            .buildMetadata().buildSessionFactory();
+
+    @Override
+    public Item add(Item item) {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        session.save(item);
+        session.getTransaction().commit();
+        session.close();
+        return item;
+    }
+
+    @Override
+    public boolean replace(int id, Item item) {
+        item.setId(id);
+        Session session = sf.openSession();
+        session.beginTransaction();
+        session.update(item);
+        session.getTransaction().commit();
+        session.close();
+        return true;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        Item item = findById(id);
+        Session session = sf.openSession();
+        session.beginTransaction();
+        session.delete(item);
+        session.getTransaction().commit();
+        session.close();
+        return true;
+    }
+
+    @Override
+    public List<Item> findAll() {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        List<Item> result = session.createQuery("from ru.job4j.tracker.model.Item").list();
+        session.getTransaction().commit();
+        session.close();
+        return result;
+    }
+
+    @Override
+    public List<Item> findByName(String key) {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        Query query = session.createQuery("from ru.job4j.tracker.model.Item where name = :key");
+        query.setParameter("key", key);
+        List result = ((org.hibernate.query.Query) query).list();
+        session.getTransaction().commit();
+        session.close();
+        return result;
+    }
+
+    @Override
+    public Item findById(int id) {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        Item result = session.get(Item.class, id);
+        session.getTransaction().commit();
+        session.close();
+        return result;
+    }
+
+    @Override
+    public void close() throws Exception {
+        StandardServiceRegistryBuilder.destroy(registry);
+    }
+}
